24d3404b28f4b8ed60a7b3ef6d0e8026be67e522
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2019-10-03 01:26:01 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    var array = expr.trim().split('');
+    var arrayWithoutSpaces = array.filter(function(elem){return elem!=' '});
+
+    //for errors
+    var opens = 0;
+    var closes = 0;
+    for (var i = 0; i<arrayWithoutSpaces.length; i++){
+    	if(arrayWithoutSpaces[i] == '('){
+    		opens++;
+    	}
+    	if(arrayWithoutSpaces[i] == ')'){
+    		closes++;
+    	}
+    }
+    var exp = arrayWithoutSpaces.join('');
+    if (closes != opens){
+    	throw "ExpressionError: Brackets must be paired";
+    }
+    else if (exp.indexOf('/0') != -1){
+    	throw "TypeError: Division by zero.";
+    }
+    else {
+    	var x ='return ' + exp;
+    	var resultFunc = new Function(x);
+    	var result = resultFunc();
+    	 return result;
+    }
+   
 }
 
 module.exports = {
