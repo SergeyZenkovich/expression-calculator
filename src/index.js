cbdc3d57aabf545a8826ffbef7dc4aca2089014a
unknown <sapocalipsys.z@gmail.com>
2020-03-04 17:40:01 +0300

    add: decision without brackets

--- src/index.js
+++ src/index.js
@@ -4,34 +4,65 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    var array = expr.trim().split('');
-    var arrayWithoutSpaces = array.filter(function(elem){return elem!=' '});
-
-    //for errors
-    var opens = 0;
-    var closes = 0;
-    for (var i = 0; i<arrayWithoutSpaces.length; i++){
-    	if(arrayWithoutSpaces[i] == '('){
-    		opens++;
-    	}
-    	if(arrayWithoutSpaces[i] == ')'){
-    		closes++;
-    	}
+    function expressionCalculator(expr) {
+        const objectWithAction = {
+            '+': (a,b)=>a+b,
+            '-': (a,b)=>a-b,
+            '*': (a,b)=>a*b,
+            '/': (a,b)=>{
+                if(b === 0){
+                    throw new Error('TypeError: Division by zero.');
+                }
+                return a/b
+            }
+    
+        }
+        let expressionWithoutSpaces = expr.split(' ').join('');
+    
+        let expressionArr = expressionWithoutSpaces.split('').map(el=>{
+            if(el === '+' ||el === '-' ||el === '*' ||el === '/' || el == '(' || el === ')'){
+                return ` ${el} `;
+            }
+            return el;
+        });
+            expressionArr = expressionArr.join('').split(' ');
+            let opensBrackets = expressionArr.filter(el=>el==='(');
+            let closesBrackets = expressionArr.filter(el=>el===')');
+            if(opensBrackets.length!==closesBrackets.length && opensBrackets.length!==0 || closesBrackets.length!==0){
+                throw new Error('ExpressionError: Brackets must be paired');
+            }
+    
+            
+        while(expressionArr.includes('+')||expressionArr.includes('-')||expressionArr.includes('*')||expressionArr.includes('/')){
+            let Action;
+            if(expressionArr.includes('*') && expressionArr.includes('/')){
+                Action = expressionArr.indexOf('*')<expressionArr.indexOf('/')? ['*', expressionArr.indexOf('*')] : ['/', expressionArr.indexOf('/')];
+            }
+            else if(expressionArr.includes('*')){
+                Action = ['*', expressionArr.indexOf('*')];
+            }
+            else if(expressionArr.includes('/')){
+                Action = ['/', expressionArr.indexOf('/')];
+            }
+            else if(expressionArr.includes('+') && expressionArr.includes('-')){
+                Action = expressionArr.indexOf('+')<expressionArr.indexOf('-')? ['+', expressionArr.indexOf('+')] : ['-', expressionArr.indexOf('-')];
+            }
+            else if(expressionArr.includes('+')){
+                Action = ['+', expressionArr.indexOf('+')];
+            }
+            else if(expressionArr.includes('-')){
+                Action = ['-', expressionArr.indexOf('-')];
+            }
+            let a = +expressionArr[Action[1]-1];
+            let b = +expressionArr[Action[1]+1];
+            let actionStr = a + Action[0] + b;
+    
+            let result = objectWithAction[Action[0]](a, b);
+            expressionArr.splice(Action[1]-1,3,result); 
+        }
+    
+        return Number(expressionArr);
     }
-    var exp = arrayWithoutSpaces.join('');
-    if (closes != opens){
-    	throw "ExpressionError: Brackets must be paired";
-    }
-    else if (exp.indexOf('/0') != -1){
-    	throw "TypeError: Division by zero.";
-    }
-    else {
-    	var x ='return ' + exp;
-    	var resultFunc = new Function(x);
-    	var result = resultFunc();
-    	 return result;
-    }
-   
 }
 
 module.exports = {
